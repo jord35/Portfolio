d11d7066f872739ca9405ad2602e9d90c7742d36
Jord35 <jordan.gillouaye@gmail.com>
2025-11-17 14:58:44 +0100

    t

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import './App.scss'
 import './index.css'
 import Header from './containers/Header'
-import AboutMe from './containers/AboutMe '
+import AboutMe from './containers/AboutMe'
 import MyLanguageCarousel from './containers/MyLanguageCarousel'
 import ProjectCards from './containers/ProjectCards'
 import SideProjectCards from './containers/SideProjectCards'
