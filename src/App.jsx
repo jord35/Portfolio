aef058500bca0612de4cd36cf7c76044bad6c6e0
Jord35 <jordan.gillouaye@gmail.com>
2025-11-17 14:55:07 +0100

    te

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 // src\App.jsx
 import './App.scss'
 import './index.css'
-import Header from './containers/header'
-import AboutMe from './containers/aboutMe'
+import Header from './containers/Header'
+import AboutMe from './containers/AboutMe '
 import MyLanguageCarousel from './containers/MyLanguageCarousel'
 import ProjectCards from './containers/ProjectCards'
 import SideProjectCards from './containers/SideProjectCards'
-import ContactMe from './containers/contactMe'
-import Footer from './containers/footer'
+import ContactMe from './containers/ContactMe'
+import Footer from './containers/Footer'
 
 function App() {
   return (
